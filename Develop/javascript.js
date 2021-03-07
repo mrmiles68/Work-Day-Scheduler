e872ed425094d4fecd61769c079b4f52b336ab4f
mark <mark@marks-MacBook-Pro.local>
2021-03-07 13:19:35 -0800

    added inputs andvariables

---
+++ Develop/javascript.js
@@ -0,0 +1,58 @@
+var timeNow = moment().format("MMMM Do YYYY")
+document.getElementById("currentDay").innerHTML = timeNow;
+console.log(timeNow);
+
+var saveNine =""
+var saveTen =""
+var saveEleven =""
+var saveTwelve =""
+var saveOne =""
+var saveTwo =""
+var saveThree =""
+var saveFour =""
+var saveFive =""
+var dataSave = function(){
+    console.log("button 9 clicked")
+}
+
+$(document).ready(function() {
+$( "#save9" ).click(function() {
+        var saveNine = $("nineTask").val();
+        alert(saveNine);
+        localStorage.hardSaveNive = saveNine;
+  });
+});
+   $( "#save10" ).click(function() {
+    var saveTen = $("tenTask").val();
+        alert(saveTen);
+  });
+  $( "#save11" ).click(function() {
+    var saveEleven = $("elevenTask").val();
+        alert(saveEleven);
+  });
+  $( "#save12" ).click(function() {
+    var saveTwelve = $("twelveTask").val();
+        alert(saveTwelve);
+  });
+  $( "#save1" ).click(function() {
+    var saveOne = $("oneTask").val();
+        alert(saveOne);
+  });
+  $( "#save2" ).click(function() {
+    var saveTwo = $("twoTask").val();
+        alert(saveTwo);
+  });
+  $( "#save3" ).click(function() {
+    var saveThree = $("threeTask").val();
+        alert(saveThree);
+  });
+  $( "#save4" ).click(function() {
+    var saveFour = $("fourTask").val();
+        alert(saveFour);
+  });
+  $( "#save5" ).click(function() {
+    var saveFive = $("fiveTask").val();
+        alert(saveFive);
+  });
+
+
